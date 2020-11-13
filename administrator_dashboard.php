5511613bfa980d598d642d98d7067fe4320a3e88
agnesjee <68498830+agnesjee@users.noreply.github.com>
2020-11-13 13:11:44 +0800

    Create administrator_dashboard.php

---
+++ administrator_dashboard.php
@@ -0,0 +1,109 @@
+<?php
+  require_once "pdo.php";
+  session_start();
+
+  //Check if we are logged in!
+  if ( ! isset($_SESSION['account'])) {
+    header("Location: index.php");
+  }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link type="text/css" rel="stylesheet" href="admin.css"/>
+</head>
+<body>
+
+<div class="sidenav">
+
+<img class = "logo" src = "logo_transparent.png" alt = "Hospial Logo" />
+<button type="button" class="btnLogout" onclick="location.href='logout.php'">Log Out</button>
+<br/><br/><br/><br/>
+
+  <a href="administrator_dashboard.php" style="color: #FFFFFF;" >Dashboard</a>
+  <a href="administrator_view_employee.php">View Employee List</a>
+</div>
+
+<div class = "headerImage">
+  <br/>
+  <span class = "pageTitle">&nbsp;Dashboard</span><br/>
+  <span class = "pageSubtitle" style="font-size: 30px; font-weight: bold; margin-left: 15px; color: #242020;">&nbsp;&nbsp;Welcome to Administrator Portal !</span>
+
+</div>
+
+<div class="main">
+
+  <h1 class = "title">View Your Profile</h1>
+
+  <?php
+
+    $sql = "SELECT emID, emName, emPhone, emEmail, emPos FROM employee WHERE emID=:emID";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+  	   ':emID' => $_SESSION['account']
+  	  ));
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    echo ('<div class="container">');
+
+    echo ('<div class="row">');
+    echo ('<div class="col-25">');
+    echo ('<p><b>Administrator ID: </b></p>');
+    echo ('</div>');
+    echo ('<div class="col-75">');
+    echo ("<p>");
+    echo ($row['emID']);
+    echo ('</p></div>');
+    echo ('</div>');
+
+    echo ('<div class="row">');
+    echo ('<div class="col-25">');
+    echo ('<p><b>Full Name: </b></p>');
+    echo ('</div>');
+    echo ('<div class="col-75">');
+    echo ("<p>");
+    echo ($row['emName']);
+    echo ('</p></div>');
+    echo ('</div>');
+
+    echo ('<div class="row">');
+    echo ('<div class="col-25">');
+    echo ('<p><b>Phone Number: </b></p>');
+    echo ('</div>');
+    echo ('<div class="col-75">');
+    echo ("<p>");
+    echo ($row['emPhone']);
+    echo ('</p></div>');
+    echo ('</div>');
+
+    echo ('<div class="row">');
+    echo ('<div class="col-25">');
+    echo ('<p><b>Email Address: </b></p>');
+    echo ('</div>');
+    echo ('<div class="col-75">');
+    echo ("<p>");
+    echo ($row['emEmail']);
+    echo ('</p></div>');
+    echo ('</div>');
+
+    echo ('<div class="row">');
+    echo ('<div class="col-25">');
+    echo ('<p><b>Position: </b></p>');
+    echo ('</div>');
+    echo ('<div class="col-75">');
+    echo ("<p>");
+    echo ($row['emPos']);
+    echo ('</p></div>');
+    echo ('</div>');
+
+    echo ('</div><br/><br/>');
+
+  ?>
+
+</div>
+</body>
+</html>
